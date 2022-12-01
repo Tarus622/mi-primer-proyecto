02cb8b8d8649648057551495bce79d96472098a0
Tarus622 <davideoliveira622@gmail.com>
2022-12-01 15:18:48 -0300

    first commit

---
+++ src/components/Web.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Banner from './Banner';
+import Products from './Products';
+import Footer from './Footer';
+
+function Web(){
+  return (
+    <div className="container">
+      <Header/>
+      <Banner/>
+      <Products/>
+      <Footer/>
+
+    </div>
+  )
+
+}
+export default Web; 
